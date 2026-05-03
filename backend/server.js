dc408f6f141f8c1320051c4f447bcab349684e0c
Emerson-1982 <emersonfreitas1982@gmail.com>
2026-05-03 17:58:23 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const path = require("path");
 
-const gpsRoutes = require("./routes/gps");
-const relatorioRoutes = require("./routes/relatorio");
-
 const app = express();
 
 // Enable JSON body parsing
@@ -12,15 +9,11 @@ app.use(express.json());
 // Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// API routes
-app.use("/api", gpsRoutes);
-app.use("/api", relatorioRoutes);
-
 // Root route
-app.get("/", (req, res) => {
+app.get("/", function (req, res) {
   res.send("Fleet Tracking Server is running ✅");
 });
 
-app.listen(3000, () => {
+app.listen(3000, function () {
   console.log("Server running on port 3000");
 });
