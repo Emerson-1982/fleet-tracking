28b6d21188c37fe68cc5d640a206bd20f7f1251f
Emerson-1982 <emersonfreitas1982@gmail.com>
2026-05-03 21:04:44 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,15 @@
 const express = require("express");
 const path = require("path");
 
-// Importa as rotas
-const gpsRoutes = require("./routes/gps");
-const relatorioRoutes = require("./routes/relatorio");
-
 const app = express();
 
-// Permite receber JSON no body (POST)
 app.use(express.json());
-
-// Serve arquivos HTML (dashboard, etc.)
 app.use(express.static(path.join(__dirname, "public")));
 
-// Rotas da API
-app.use("/api", gpsRoutes);
-app.use("/report", relatorioRoutes);
-
-// Rota principal
 app.get("/", function (req, res) {
   res.send("Fleet Tracking Server is running ✅");
 });
 
-// Inicia o servidor
 app.listen(3000, function () {
   console.log("Server running on port 3000");
-});`
+});
